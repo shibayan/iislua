c5310bb912ba2e768f8799c33d4735b4c4f02402
shibayan <shiba-yan@outlook.com>
2015-06-24 00:08:59 +0900

    add sample scripts

---
+++ sample/html.lua
@@ -0,0 +1,2 @@
+iis.resp.set_header("Content-Type", "text/html")
+iis.print("<html><body><h1>Hello, World</h1></body></html>")
---
+++ sample/redirect.lua
@@ -0,0 +1,5 @@
+if math.random() > 0.5 then
+    iis.redirect("http://buchizo.wordpress.com/")
+else
+    iis.redirect("http://blog.engineer-memo.com/")
+end
