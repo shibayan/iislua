6dc35b13ab3e0a7752683a8ed990c4999b0a9b4c
shibayan <shiba-yan@outlook.com>
2016-06-24 01:32:49 +0900

    update to Lua 5.3.3

--- build.cmd
+++ build.cmd
@@ -1,4 +1,6 @@
 @echo off
 
+set PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin
+
 msbuild src\iislua.sln /p:Platform=Win32 /p:Configuration=Release /m
 msbuild src\iislua.sln /p:Platform=x64 /p:Configuration=Release /m
